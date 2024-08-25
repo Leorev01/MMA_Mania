a9e9541135593e258e1e6396a97b6b1e4ca673c1
leorev01 <eric@example.com>
2024-08-25 13:15:09 +0100

    Adjusts fetch requests with an api URL for website publishing

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,9 +1,11 @@
 import classes from "../pages/MainShop.module.css";
 import {Link} from "react-router-dom";
 
+const apiURL = process.env.REACT_APP_API_URL || "https://mma-mania-backend.onrender.com";
+
 function Product({product}){
     return(
-        <Link to={`/shop/${product.id}`} className={classes.product}>
+        <Link to={`${apiURL}/shop/${product.id}`} className={classes.product}>
             <img src={product.image} alt="product"/>
             <div className="product-info">
                 <p>{product.name}</p>
